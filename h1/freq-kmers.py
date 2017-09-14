2e2f83a41b7bc4380060fae9ab000a1647e62623
srnesdoly <srnesdoly@gmail.com>
2017-09-14 17:49:50 -0400

    Add solution to most frequent kmers
    The particular solution implemented has a worst-case time complexity of O(n^2).

---
+++ h1/freq-kmers.py
@@ -0,0 +1,39 @@
+# Frequent Words Problem
+#
+# Compute the most frequent kmers within a string. A kmer is defined as a
+# substring of length k. The implemented algorithm has a worst-case time
+# complexity of O(n^2); this will vary depending on the collision resolution
+# technique used when filling the hashtable with kmers. The space complexity is
+# determined by how large the hashtable is; thus, it is in O(n).
+#
+# CISC471
+# Sean Nesdoly
+# 2017-09-14
+
+# Find the most frequent kmers in a string
+def get_most_frequent_kmers(text, k):
+	kmers = parse_kmers(text, k)
+	print(kmers)
+	max_kmer = max(kmers.values())
+
+	for token in kmers:
+		if (kmers[token] == max_kmer):
+			print(token)
+
+# Create a dictionary of unique kmers where each value denotes the count of
+# how often that kmer appears within the given string
+def parse_kmers(text, k):
+	n = len(text)-k+1 # number of kmers
+	kmers = {text[0:k]: 1}
+
+	for i in range(1, n):
+		token = text[i:i+k]
+		if (token in kmers):
+			kmers[token] +=1
+		else:
+			kmers[token] = 1
+
+	return kmers
+
+# sample input
+get_most_frequent_kmers("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4)
