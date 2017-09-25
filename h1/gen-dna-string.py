a665f5a124f053005bbad3939890b8cb27a0dfe8
srnesdoly <srnesdoly@gmail.com>
2017-09-25 16:47:23 -0400

    Convert script to function

--- h1/gen-dna-string.py
+++ h1/gen-dna-string.py
@@ -1,12 +1,15 @@
 # Generate all possible DNA strings of length k
 #
+# input:	integer k denoting the length of DNA strings to generate
+# output:	a list of tuples of all possible DNA strings of length k 
+#	
 # CISC471
 # Sean Nesdoly
 # 2017-09-19
 
 import itertools
 
-k = 3 # length of DNA string to generate 
-
-alphabet = ['A','C','G','T']
-allKmers = [i for i in itertools.product(alphabet, repeat=k)]
+def gen_dna_string(k):
+	alphabet = ['A','C','G','T']
+	allKmers = [i for i in itertools.product(alphabet, repeat=k)]
+	return allKmers
